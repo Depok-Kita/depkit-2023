ee0fb994825956991a7416bdc15d68d7d76d4978
CyberSleeper <mahartha.gemilang@gmail.com>
2023-08-31 01:01:42 +0700

    [FIX] UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,51 +3,42 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export const Article = () => {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-    <div className="w-[1080px] h-[486px] relative">
-      <div className="left-[403px] top-0 absolute text-center text-blue-950 text-[64px] font-bold">Artikel</div>
-      <div className="w-1 h-[372px] left-[538px] top-[114px] absolute bg-zinc-300 rounded-[20px]"></div>
-      <div className="w-[494px] h-[165px] left-0 top-[114px] absolute">
-        <div className="w-[494px] h-[165px] left-0 top-0 absolute bg-white rounded-[20px] shadow"></div>
-        <div className="w-[150px] h-[115.30px] left-[20px] top-[24.85px] absolute bg-zinc-300 rounded-[20px]"></div>
-        <div className="h-[114.31px] left-[186px] top-[24.85px] absolute">
-          <div className="w-72 h-[56.66px] left-0 top-[21.87px] absolute text-blue-950 text-base font-bold leading-tight">Rapat Koordinasi Lomba Posyandu dan Kader Posyandu Tingkat Provinsi Jawa Barat</div>
-          <div className="w-72 h-[27.83px] left-0 top-[86.48px] absolute text-neutral-400 text-xs font-semibold leading-[14.40px]">Dinas Kesehatan (Dinkes) Kota Depok mengadakan rapat Koordinasi Lomba Posyandu...</div>
-          <div className="w-[114px] h-[13.92px] left-0 top-0 absolute text-neutral-400 text-xs font-semibold leading-[14.40px]">Selasa, 27 Mei 2023</div>
+    <div className='shadow-xl rounded-2xl w-full bg-white flex px-5 py-6 gap-4 border-blue-950 border-solid border-2'>
+      <div className="align-middle w-52 max-w-lg h-28 bg-zinc-300 rounded-2xl"/>
+      <div className='flex flex-col gap-2'>
+        <div className='text-neutral-400 text-xs font-semibold'>
+          Selasa, 27 Mei 2023
         </div>
-      </div>
-      <div className="w-[494px] h-[165px] left-0 top-[321px] absolute">
-        <div className="w-[494px] h-[165px] left-0 top-0 absolute bg-white rounded-[20px] shadow"></div>
-        <div className="w-[150px] h-[115.30px] left-[20px] top-[24.85px] absolute bg-zinc-300 rounded-[20px]"></div>
-        <div className="h-[114.31px] left-[186px] top-[24.85px] absolute">
-          <div className="w-72 h-[56.66px] left-0 top-[21.87px] absolute text-blue-950 text-base font-bold leading-tight">Rapat Koordinasi Lomba Posyandu dan Kader Posyandu Tingkat Provinsi Jawa Barat</div>
-          <div className="w-72 h-[27.83px] left-0 top-[86.48px] absolute text-neutral-400 text-xs font-semibold leading-[14.40px]">Dinas Kesehatan (Dinkes) Kota Depok mengadakan rapat Koordinasi Lomba Posyandu...</div>
-          <div className="w-[114px] h-[13.92px] left-0 top-0 absolute text-neutral-400 text-xs font-semibold leading-[14.40px]">Selasa, 27 Mei 2023</div>
+        <div className='text-blue-950 text-base font-bold leading-tight'>
+          Rapat Koordinasi Lomba Posyandu dan Kader Posyandu Tingkat Provinsi Jawa Barat
         </div>
-      </div>
-      <div className="w-[494px] h-[165px] left-[586px] top-[114px] absolute">
-        <div className="w-[494px] h-[165px] left-0 top-0 absolute bg-white rounded-[20px] shadow"></div>
-        <div className="w-[150px] h-[115.30px] left-[20px] top-[24.85px] absolute bg-zinc-300 rounded-[20px]"></div>
-        <div className="h-[114.31px] left-[186px] top-[24.85px] absolute">
-          <div className="w-72 h-[56.66px] left-0 top-[21.87px] absolute text-blue-950 text-base font-bold leading-tight">Rapat Koordinasi Lomba Posyandu dan Kader Posyandu Tingkat Provinsi Jawa Barat</div>
-          <div className="w-72 h-[27.83px] left-0 top-[86.48px] absolute text-neutral-400 text-xs font-semibold leading-[14.40px]">Dinas Kesehatan (Dinkes) Kota Depok mengadakan rapat Koordinasi Lomba Posyandu...</div>
-          <div className="w-[114px] h-[13.92px] left-0 top-0 absolute text-neutral-400 text-xs font-semibold leading-[14.40px]">Selasa, 27 Mei 2023</div>
-        </div>
-      </div>
-      <div className="w-[494px] h-[165px] left-[586px] top-[321px] absolute">
-        <div className="w-[494px] h-[165px] left-0 top-0 absolute bg-white rounded-[20px] shadow"></div>
-        <div className="w-[150px] h-[115.30px] left-[20px] top-[24.85px] absolute bg-zinc-300 rounded-[20px]"></div>
-        <div className="h-[114.31px] left-[186px] top-[24.85px] absolute">
-          <div className="w-72 h-[56.66px] left-0 top-[21.87px] absolute text-blue-950 text-base font-bold leading-tight">Rapat Koordinasi Lomba Posyandu dan Kader Posyandu Tingkat Provinsi Jawa Barat</div>
-          <div className="w-72 h-[27.83px] left-0 top-[86.48px] absolute text-neutral-400 text-xs font-semibold leading-[14.40px]">Dinas Kesehatan (Dinkes) Kota Depok mengadakan rapat Koordinasi Lomba Posyandu...</div>
-          <div className="w-[114px] h-[13.92px] left-0 top-0 absolute text-neutral-400 text-xs font-semibold leading-[14.40px]">Selasa, 27 Mei 2023</div>
+        <div className='text-neutral-400 text-xs font-semibold'>
+          Dinas Kesehatan (Dinkes) Kota Depok mengadakan rapat Koordinasi Lomba Posyandu...
         </div>
       </div>
     </div>
+  )
+}
+
+export default function Home() {
+  return (
+    <main className='px-20'>
+      <div className='text-center text-blue-950 font-bold text-[64px]'>
+        Artikel
+      </div>
+      <div className='gap-10 flex h-max items-center justify-center'>
+        <div className='flex flex-col gap-10'>
+          <Article/>
+          <Article/>
+        </div>
+        <div className="w-3 h-96 bg-zinc-300 rounded-2xl"/>
+        <div className='flex flex-col gap-10'>
+          <Article/>
+          <Article/>
+        </div>
+      </div>
     </main>
   )
 }
