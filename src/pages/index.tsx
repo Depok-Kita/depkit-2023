a2b805511b2be38b813819ff502e4b0d64ed3c37
Maurice Yang <mauriceyangmy28@gmail.com>
2023-09-01 00:56:02 +0700

    fix: Fixing timeline styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,10 +12,10 @@ export const Home = () => {
       className={`bg-[#F2FAFE]`}
     >
       <Hero />
-      <section id="sosvent" className='relative mt-5 p-10 min-h-screen'>
+      <section id="sosvent" className='relative mt-5 p-10'>
         <aside className="p-5 shadow-md bg-white rounded-lg h-full">
           <h1 className='text-4xl font-bossa md:text-7xl font-bold text-blue-950 text-center'>Social Event Timeline</h1>
-          <div className='p-5 mt-10 overflow-x-scroll'>
+          <div className='p-5 my-10 overflow-x-auto'>
             <img src="/timeline.svg" className='min-w-[700px] w-full' />
           </div>
         </aside>
